ca2bdd03803ac175fbc6a43091c431ce4ec76d8c
GLRoylance <GLRoylance@gmail.com>
2024-12-29 13:19:45 -0800

    add simple JUnit tests

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/SillyTests.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import static org.junit.Assert.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+// import org.junit.jupiter.api.Test;
+
+public class SillyTests {
+    @Test
+    public void additionTest() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void additionFailTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
